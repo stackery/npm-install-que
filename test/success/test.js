7cadc322eb914d98cf4eeccd3ff1b3125d745c47
Michael Wuergler <wuergler@gmail.com>
2016-03-12 11:25:44 +0000

    setup success test

--- test/success/test.js
+++ test/success/test.js
@@ -1,6 +1,27 @@
 import test from 'ava';
-import fn from './';
+var spawn = require('cross-spawn-async');
+var rimraf = require('rimraf');
+var que = require('');
+// var pify = require('pify');
+test.before(() => {
+	rimraf.sync('node_modules');
+});
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test.cb('successfully installs one at a time', t => {
+	t.plan(1);
+
+	var child = spawn('node', ['../../index.js'], {stdio: 'inherit'});
+
+	child.on('error', function (err) {
+		t.fail(err);
+	});
+
+	child.on('exit', async function (exitCode) {
+		if (exitCode === 0) {
+			t.true(false);
+		} else {
+			t.fail('exitCode: ' + exitCode);
+		}
+		que();
+	});
 });
